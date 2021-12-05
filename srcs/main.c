d0be41d19b7c86389030dd8329984bf1e9351410
전상훈 <jeonsanghun@jeonsanghun-ui-MacBookAir.local>
2021-12-05 11:46:44 +0900

    main문에 argc, argv, envp파라미터 입력 추가

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,22 @@
 /*   By: sangjeon <sangjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:59:57 by sangjeon          #+#    #+#             */
-/*   Updated: 2021/12/05 09:02:18 by sangjeon         ###   ########.fr       */
+/*   Updated: 2021/12/05 11:46:04 by sangjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main()
+int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	/* 리다이렉션이나 파이프가 걸리면 여러개의 명령어를 처리해야해서
 	리스트로 명령어들을 구현 */
 	t_list	*cmd_list;
 
+	if (argc != 1)
+		return (0);
+	(void)argv;
 	cmd_list = 0;
 	signal(SIGINT, sig_handler);
 	signal(SIGQUIT, sig_handler);

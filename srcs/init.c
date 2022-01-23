23c79b056a17e29eea238bc3b96fb119b2b4ab89
전상훈 <jeonsanghun@jeonsanghun-ui-MacBookAir.local>
2022-01-23 22:06:55 +0900

    fix init env parsing

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: sangjeon <sangjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 21:11:45 by sangjeon          #+#    #+#             */
-/*   Updated: 2022/01/20 23:49:00 by sangjeon         ###   ########.fr       */
+/*   Updated: 2022/01/23 22:05:01 by sangjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 void	envp_to_list(char **envp)
 {
-	char	**one_env_arr;
+	int		len;
 	t_env	*env;
 	t_list	*env_list;
 
 	while (*envp)
 	{
-		one_env_arr = ft_split(*envp, '=');
-		if (!one_env_arr)
-			init_err();
 		env = malloc(sizeof(t_env));
 		if (!env)
 			init_err();
-		env->key = one_env_arr[0];
-		env->val = one_env_arr[1];
+		len = length_to_equ(*envp);
+		env->key = ft_strndup(*envp, len);
+		env->val = ft_strdup(*envp + len + 1);
+		if (!env->key || !env->val)
+			init_err();
 		env->flag = 0;
 		env_list = ft_lstnew(env);
 		if (!env_list)
 			init_err();
 		ft_lstadd_back(&g_vars.env_list, env_list);
-		free(one_env_arr);
 		envp++;
 	}
 }
